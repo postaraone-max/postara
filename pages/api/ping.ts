a63523fcd118e93d4366a83df3030340da5b9e9b
postaraone-max <postaraone@gmail.com>
2025-08-30 22:38:42 +0200

    Create ping.ts

---
+++ pages/api/ping.ts
@@ -0,0 +1,5 @@
+// pages/api/ping.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ ok: true, via: "pages-api" });
+}
