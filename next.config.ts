c048f66abcec34307545ea783ad6a25dc6d75f28
postaraone-max <postaraone@gmail.com>
2025-08-30 22:55:32 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // DO NOT set output: 'export' — that disables API routes.
-  reactStrictMode: true,
+  // DO NOT set `output: 'export'` — that disables API routes.
+  reactStrictMode: true
 };
 
 export default nextConfig;
-
