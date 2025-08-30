9780b16f09c7d4d5f69b269bdc07a3bfff4d7dff
Lokman Kanilany <kanilany@gmail.com>
2025-08-31 01:40:42 +0200

    migrate next.config.ts to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // DO NOT set `output: 'export'` – it disables API routes.
+  reactStrictMode: true,
+};
+
+export default nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-// next.config.ts
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // DO NOT set `output: 'export'` — that disables API routes.
-  reactStrictMode: true
-};
-
-export default nextConfig;
